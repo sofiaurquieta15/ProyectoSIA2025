cdc55f393eb4410657406b0db0341ad008af9edd
MillarayZalazarM <millaray.zalazar@alumnos.ucn.cl>
2025-10-31 22:05:28 -0300

    Update Models y Admin CasosPacientes
    creación de tablas Paciente, Diagnostico y Respuesta
    
    Co-Authored-By: constanzatorres19 <240232852+constanzatorres19@users.noreply.github.com>

--- ProyectoKine/applications/casospacientes/models.py
+++ ProyectoKine/applications/casospacientes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from applications.cursosdocente.models import Curso
 
-# Create your models here.
+class Paciente(models.Model):
+    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nombre_caso = models.CharField("Nombre Caso", max_length=200)
+    tipo_de_caso = models.CharField("Tipo de Caso", max_length=150)
+    descripcion = models.TextField("Descripción del caso")
+    url_video = models.URLField("URL video")
+    pregunta = models.TextField("Pregunta")
+
+    def __str__(self):
+        return f"{self.nombre_caso} <{self.id}>"
+
+class Diagnostico(models.Model):
+    id_paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    fecha_creacion = models.DateField("Fecha de Creación",auto_now_add=True)
+    nombre_formulario = models.CharField("Nombre Formulario",max_length=150)
+    descripcion = models.TextField("Descripción del formulario") 
+
+    def __str__(self):
+        return f"Diagnóstico {self.id} para {self.id_paciente.nombre_caso}"
+
+class Respuesta(models.Model):
+    id_paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    id_diagnostico = models.ForeignKey(Diagnostico, on_delete=models.CASCADE)
+    correcta_incorrecta = models.BooleanField()
+    respuesta_correcta = models.TextField("Respuesta correcta")
+    retroalimentacion = models.TextField("Retroalimentación")
+
+    def __str__(self):
+        return f"Respuesta {self.id} para {self.id_paciente.nombre_caso}"
