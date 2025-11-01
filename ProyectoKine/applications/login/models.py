8d45d48538e1d369766337e50daf7e1f504c3189
melany <melany.torres@alumnos.ucn.cl>
2025-10-31 21:43:13 -0300

    Update models.py

--- ProyectoKine/applications/login/models.py
+++ ProyectoKine/applications/login/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Docente(models.Model):
+    nombre_docente=models.CharField("Nombre Docente",max_length=150,null=False)
+    correo_docente = models.EmailField("Email Docente", unique=True)
+    passw_docente = models.CharField("Contraseña Docente", max_length=25, null=False)
+
+    def __str__(self):
+        return f"{self.nombre_docente} <{self.correo_docente}>"
+
+class Estudiante(models.Model):
+    nombre = models.CharField("Nombre Estudiante", max_length=150, null=False)
+    correo_institucional = models.EmailField("Email Estudiante", unique=True)
+    passw_estudiante = models.CharField("Contraseña Estudiante", max_length=25, null=False)
+
+    def __str__(self):
+        return f"{self.nombre} <{self.correo_institucional}>"
