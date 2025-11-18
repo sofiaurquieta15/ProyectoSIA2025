73af1963c010d5073bfc1143990300cefba2652a
jalilahure28 <jalil.ahure@alumnos.ucn.cl>
2025-11-17 23:12:59 -0300

    View de estudiante 1.2
    Se incluye etapas 2 y 3.

--- ProyectoKine/ProyectoKine/settings/base.py
+++ ProyectoKine/ProyectoKine/settings/base.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -44,7 +44,7 @@ ROOT_URLCONF = "ProyectoKine.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'ProyectoKine' / 'templates'],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
