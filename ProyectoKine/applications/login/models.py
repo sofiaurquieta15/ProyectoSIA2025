b4f7d44d377f9fed8d0f11f61b7ff6af07f5d262
MillarayZalazarM <millaray.zalazar@alumnos.ucn.cl>
2025-11-30 21:15:10 -0300

    Update Login
    Se agregó la verificación de correos de estudiantes y docentes.

--- ProyectoKine/applications/login/models.py
+++ ProyectoKine/applications/login/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+def validar_correo_alumno(value):
+    if not value.endswith("@alumnos.ucn.cl"):
+        raise ValidationError("El correo debe terminar en @alumnos.ucn.cl")
+    
+def validar_correo_docente(value):
+    dominios_permitidos = ["@ucn.cl", "@ce.ucn.cl"]
+    if not any(value.endswith(dominio) for dominio in dominios_permitidos):
+        raise ValidationError("El correo debe ser @ucn.cl o @ce.ucn.cl")
+
 
 class Docente(models.Model):
     nombre_docente = models.CharField("Nombre Docente",max_length=150,null=False, blank=True)
-    correo_docente = models.EmailField("Email Docente", unique=True)
+    correo_docente = models.EmailField("Email Docente", unique=True, validators=[validar_correo_docente])
     passw_docente = models.CharField("Contraseña Docente", max_length=25, null=False, default='123456')
     last_login = models.DateTimeField(null=True, blank=True)
 
@@ -16,7 +27,7 @@ class Docente(models.Model):
 
 class Estudiante(models.Model):
     nombre = models.CharField("Nombre Estudiante", max_length=150, null=False, blank=True)
-    correo_institucional = models.EmailField("Email Estudiante", unique=True)
+    correo_institucional = models.EmailField("Email Estudiante", unique=True, validators=[validar_correo_alumno])
     passw_estudiante = models.CharField("Contraseña Estudiante", max_length=25, null=False, default='123456')
     last_login = models.DateTimeField(null=True, blank=True)
 
@@ -24,7 +35,7 @@ class Estudiante(models.Model):
         return f"{self.nombre} <{self.correo_institucional}>"
     
     class Meta:
-        verbose_name = "Estudiantes"
+        verbose_name = "Estudiante"
         verbose_name_plural = "Estudiantes"
         ordering = ['nombre']
 
